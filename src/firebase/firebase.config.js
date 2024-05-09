6c8c478fdbb0ed2782676613f6cadb80a1a283b7
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-05-10 00:20:54 +0600

    Environment variable used to hide firebase credentials

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyARoTmLBT5oRUvAZEDfcZ-LWaxQlX_tjjs",
-  authDomain: "heal-hive.firebaseapp.com",
-  projectId: "heal-hive",
-  storageBucket: "heal-hive.appspot.com",
-  messagingSenderId: "700165909354",
-  appId: "1:700165909354:web:035b8c5399d3ddbd175313"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId:import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId:import.meta.env.VITE_messagingSenderId,
+    appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
